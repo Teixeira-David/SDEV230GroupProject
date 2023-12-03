50c29571ee40f52fb84415bda1b4741e4236c52e
Lucas Landis <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2023-12-03 13:41:30 -0600

    Update Geographic.h

--- Geographic.h
+++ Geographic.h
@@ -12,9 +12,24 @@ Date of last update: 11/30/2023 8:54AM
 #include <sstream> 
 
 
-// Geographic section of census
-class Geographic {
+// Geographic class
+class geographicClass {
     public: 
         string state; // state, territory, embassy, or base
         string popGeo; // rural, Urban, or suburban
+        
+        // Default constructor
+        geographicClass() {}
+
+        // Constructor parameters
+        geographicClass(string popGeo, int state)
+            : strPopGeo(popGeo), intState(state), {}
+
+        // Gets the Rural, Urban, or suburban / popGeo
+        void get_Population_Geo() {
+            int input;
+            cout << "\n Enter 1 for Rural, 2 for Urban, 3 for Suburban: ";
+            cin >> input;
+        };
+
 };
