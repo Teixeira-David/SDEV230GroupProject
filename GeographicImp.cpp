e0f9298e0851d74490907b845d06ec4a56068e32
Lucas Landis <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2023-12-09 14:32:25 -0600

    Update GeographicImp.cpp

--- GeographicImp.cpp
+++ GeographicImp.cpp
@@ -1,9 +1,17 @@
 // Geographic class Implementation file
-/*
+/*  
 Author: Lucas Landis
-Date of creation: 
+Date of creation: 12/9/2023 2:30PM
 */
 
-#include 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream> 
+#include <iomanip>
+#include <limits>
+#include "Geographic.h"
 
 using namespace std;
+
+geographicClass
