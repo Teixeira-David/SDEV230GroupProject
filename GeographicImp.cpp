e90339881126cfc2f56fdedf899966fe8d19644a
Lucas Landis <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2023-12-11 09:28:37 -0600

    Update GeographicImp.cpp

--- GeographicImp.cpp
+++ GeographicImp.cpp
@@ -22,4 +22,33 @@ geographicClass::geographicClass()
 geographicClass::geographicClass(int rurality, string state)
     : intRurality(rurality), strState(state) {}
 
+// Enter Geo inputs function
 
+
+// setter functions
+void set_Rurality() {
+    int intInput;
+    bool valid = false;
+    
+    while (!(valid)) {
+        cout << "\nEnter 1 for Rural, 2 for Urban, 3 for Suburban: ";
+        cin >> intInput;
+
+        // Check if input is valid
+        if (intInput == 1 || intInput == 2 || intInput == 3) {
+            valid = true;
+            intRurality = intInput;
+        } 
+        else {
+            cout << "\nError: Invalid input. Please enter 1, 2, or 3.";
+            cin.clear(); // Clear error flags
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+        }
+
+    }
+};
+
+// getter functions
+
+
+// print functions
