65da49ddee30b00a653f44bd16605fa952d99e98
Corvus-1 <115851555+Corvus-1@users.noreply.github.com>
2023-12-11 20:35:48 -0500

    Economics.h

--- Economics.h
+++ Economics.h
@@ -1,70 +1,22 @@
+// Economics.h
+
+#ifndef Economics_H
+#define Economics_H
+
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <sstream> 
 
-
-// Econcomic class
-class economicClass {
+class Economics {
 public:
-    double grossIncome; // User enters their yearly Gross Income
-    int employStatus; // Have user enter their current employment status. Part-time, Full-time, or unemployed
-    
-    // Default constructor
-    economicClass() {}
+    // Function to get the user's gross income
+    double getGrossIncome() const;
 
-    // Constructor parameters
-    economicClass(double grossIncome, int employStatus)
-        : doublegrossIncome(grossIncome), intemploystatus(employStatus), {}
+    // Function to get the user's employment status
+    std::string getEmploymentStatus() const;
 
-        // Gets the users employment status
-        void get_Employment_Status() {
-        int intInput;
-        bool valid = false;
-            while(!(valid)) {
-                cout << "\n Enter 1 for Full-time, 2 for Part-time, 3 for self-employed, 4 for unemployed: ";
-                cin >> intInput;
-
-                if(inInput == 4 || intInput == 3 || intInput == 2 || intInput == 1){
-                    valid = true;
-                    intEmployStatus = intInput;
-                }
-                else{
-                    cout << "\nError: Invalid input. Please use the 1, 2, 3, or 4 number keys.";
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                }
-            }
-    };
-
-    // Gets the Users gross income
-    void get_Gross_Income() {
-        double input;
-        cout << "\n Please enter your yearly gross income : ";
-        cin >> input;
-    };
-
-    void display_employStatus() {
-        string strOutput
-        switch(intEmployStatus)
-            case 1:
-                strOutput = "Full-time";
-                cout << "\nEmployStatus is: " << strOutput;
-                break;
-       
-            case 2:
-                strOutput = "Part-time";
-                cout << "\nEmployStatus is: " << strOutput;
-                break;
-
-            case 3:
-                strOutput = "Self-Employed";
-                cout << "\nEmployStatus is: " << strOutput;
-                break;
-
-            case 4:
-                strOutput = "Unemployed";
-                cout << "\nEmployStatus is: " << strOutput;
-                break;
-    }
+private:
+    // Helper function to validate numeric input
+    double getNumericInput(const std::string& prompt) const;
 };
+
+#endif // Economics_H
