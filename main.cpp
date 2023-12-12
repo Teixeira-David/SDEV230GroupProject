a4a87f4aeeb0f26e97a655969f103cceaa192a0c
Corvus-1 <115851555+Corvus-1@users.noreply.github.com>
2023-12-11 21:09:38 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+// main.cpp
+
+#include "Economics.h"
 
 int main() {
-    // Print "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
+    Economics user;
+
+    user.setGrossIncome();
+    user.setEmploymentStatus();
+
+    // Display user information
+    user.displayEconomics();
+
     return 0;
 }
