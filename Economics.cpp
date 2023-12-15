59df09d48432264e8e8e01c8126c664885743a7e
Corvus-1 <115851555+Corvus-1@users.noreply.github.com>
2023-12-15 18:53:40 -0500

    Economics.cpp

--- Economics.cpp
+++ Economics.cpp
@@ -1,21 +1,30 @@
+// Author: Nicholas Moffitt
+// Project: CensusHub Group Final Project
+// Course: SDEV-230
+
+// Economics class Implementation file
+
 // Economics.cpp
 
+// Include the header for the class
 #include "Economics.h"
 
+// Constructors with parameters
+// function to set the users yearly income
 void Economics::setGrossIncome() {
     grossIncome = getNumericInput("Enter your gross income: ");
 }
-
+// Function to set employment status
 void Economics::setEmploymentStatus() {
     std::cout << "Enter your employment status (e.g., Full-time, Part-time, unemployed, self-employed): ";
     std::getline(std::cin, employmentStatus);
 }
-
+// function to display the users employment status and yearly income
 void Economics::displayEconomics() const {
     std::cout << "Gross Income: $" << grossIncome << std::endl;
     std::cout << "Employment Status: " << employmentStatus << std::endl;
 }
-
+//function to put the users employment status and gross income into a text file
 void Economics::saveToFile(const std::string& filename) const {
     std::ofstream outFile(filename);
     if (outFile.is_open()) {
@@ -28,7 +37,7 @@ void Economics::saveToFile(const std::string& filename) const {
         std::cerr << "Error: Unable to open file " << filename << " for writing." << std::endl;
     }
 }
-
+// clears the users input for the next user
 double Economics::getNumericInput(const std::string& prompt) const {
     double value;
     std::cout << prompt;
